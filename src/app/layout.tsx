eed726f46aef4a97ec16d780e8f16305dd05e495
Javier Diaz Garzon <javierdiazgarzon@gmail.com>
2025-03-03 15:25:45 +0100

    feat: adjust react query config and cache (MVR005)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,16 @@
 "use client";
 
-import { ReactNode, useState } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode } from "react";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
 import { store } from "@/store";
 import { Header } from "@/components/Header";
+import { queryClient } from "../lib/reactQueryClient";
 import "@/styles/globals.scss";
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: ReactNode;
-}>) {
-  const [queryClient] = useState(() => new QueryClient());
-
+}: Readonly<{ children: ReactNode }>) {
   return (
     <html lang="en">
       <body>
